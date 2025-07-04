dd2345220320f7e8bb485ee26a9a1b1ece4953f6
Ali Roohan <aliroohan321@gmail.com>
2025-07-04 18:51:58 +0500

    updated email details

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import mail from "./controllers/mail.js";
+// import mail from "./controllers/mail.js";
 import mail2 from "./controllers/mail2.js";
 import dotenv from "dotenv";
 
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.use("/mail", mail);
+// app.use("/mail", mail);
 app.use("/mail2", mail2);
 
 app.listen(port, () => {
--- controllers/mail2.js
+++ controllers/mail2.js
@@ -8,7 +8,34 @@ const router = express.Router();
 
 // POST /send-email
 router.post('/send-email', async (req, res) => {
-  const { to, subject, message } = req.body;
+  const { name, email, country, phone, message } = req.body;
+
+  const html_template = `
+  <!DOCTYPE html>
+    <html lang='en'>
+    <head>
+      <meta charset='UTF-8'>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    </head>
+    <body style='font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f4f4f4;'>
+      <div style='max-width: 600px; margin: 20px auto; background: #ffffff; border-radius: 8px; overflow: hidden; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);'>
+        <div style='background-color: #A020F0; color: #ffffff; text-align: center; padding: 20px;'>
+          <h1 style='margin: 0; font-size: 24px;'>Contact Form Submission</h1>
+        </div>
+        <div style='padding: 20px;'>
+          <h2 style='color: #333333;'> <span style='font-weight: bold; color: #4CAF50;'>${name}</span>!</h2>
+          <p style='margin: 10px 0; color: #555555;'><strong>Email:</strong> ${email}</p>
+          <p style='margin: 10px 0; color: #555555;'><strong>Country:</strong> ${country}</p>
+          <p style='margin: 10px 0; color: #555555;'><strong>Phone:</strong> ${phone}</p>
+          <p style='margin: 10px 0; color: #555555;'><strong>Message:</strong> ${message}</p>
+        </div>
+        <div style='background-color: #f4f4f4; text-align: center; padding: 10px; font-size: 12px; color: #999999;'>
+          <p style='margin: 0;'>&copy; 2024 TechAdvancer. All rights reserved.</p>
+        </div>
+      </div>
+    </body>
+    </html>
+    `
 
   try {
     const transporter = nodemailer.createTransport({
@@ -23,9 +50,9 @@ router.post('/send-email', async (req, res) => {
 
     const info = await transporter.sendMail({
       from: process.env.EMAIL,
-      to,
-      subject,
-      text: message,
+      to: process.env.EMAIL_TO  ,
+      subject: 'Contact Form Submission',
+      html: html_template,
     });
 
     res.status(200).json({
